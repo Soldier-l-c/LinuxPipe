0ee3aa3a660993e1e8d8f686546f3f14c319b8cb
soldier <1870056213@qq.com>
2021-12-03 17:09:00 +0800

    update bulid.sh

--- bulid.sh
+++ bulid.sh
@@ -1,1 +1,1 @@
-g++ -Wall -g -o pipe.app main.cpp CommunicationPipe.cpp PipeRequestCallBack.cpp PipeResponseCallBack.cpp util.cpp  -lpthread
+ g++ -Wall -g -o pipe.app main.cpp CommunicationPipe.cpp PipeRequestCallBack.cpp PipeResponseCallBack.cpp util.cpp  -lpthread
